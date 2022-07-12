e9d5075b065a450d977db5ecc9c6bf9c17fbd32d
Caleb Waymeyer <cjwaymeyer@gmail.com>
2022-07-12 13:42:16 -0400

    initial cdk commit

--- procyon/procyon_stack.py
+++ procyon/procyon_stack.py
@@ -1,8 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
+    aws_lambda,
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
@@ -14,13 +13,7 @@ class ProcyonStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "ProcyonQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "ProcyonTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        post_lambda = aws_lambda.Function(self, "post_lambda", 
+                                        runtime=aws_lambda.Runtime.PYTHON_3_8, 
+                                        handler="lambda_function.lambda_handler",
+                                        code=aws_lambda.Code.from_asset("./src/lambdas/post"))
--- tests/unit/test_procyon_stack.py
+++ tests/unit/test_procyon_stack.py
@@ -3,19 +3,9 @@ import aws_cdk.assertions as assertions
 from procyon.procyon_stack import ProcyonStack
 
 
-def test_sqs_queue_created():
+def test_lambda_created():
     app = core.App()
     stack = ProcyonStack(app, "procyon")
     template = assertions.Template.from_stack(stack)
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
-    })
-
-
-def test_sns_topic_created():
-    app = core.App()
-    stack = ProcyonStack(app, "procyon")
-    template = assertions.Template.from_stack(stack)
-
-    template.resource_count_is("AWS::SNS::Topic", 1)
+    template.resource_count_is("AWS::Lambda::Function", 1)
